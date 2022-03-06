533bd570b53cbcea95cac407f12d5d8430478131
chamibia <b.chami39@gmail.com>
2022-03-06 12:38:04 -0500

    finding error

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,39 +1,91 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
+
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
 describe('Nav component', () => {
-    it('renders', () => {
-      render(<Nav />);
-    });
-  
-    it('matches snapshot', () => {
-      const { asFragment } = render(<Nav />);
-      
-      expect(asFragment()).toMatchSnapshot();
-    });
+  it('renders', () => {
+    render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
   })
-  
-  describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-    const { getByLabelText } = render(<Nav />);
-  
+
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
+
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+
     // eslint-disable-next-line testing-library/prefer-screen-queries
     expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
-  })  
-  
-  describe('links are visible', () => {
-    it('inserts text into the links', () => {
-      const { getByTestId } = render(<Nav />);
-  
-      // eslint-disable-next-line testing-library/prefer-screen-queries
-      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-      // eslint-disable-next-line testing-library/prefer-screen-queries
-      expect(getByTestId('about')).toHaveTextContent('About me');
-    });
-  
-  })
+  });
+})
+
+describe('links are visible', () => {
+  it('inserts text into the home link', () => {
+    const { getByTestId } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+})
+
+describe('onClick events', () => {
+  it('calls the click handler when clicked', () => {
+    const { getByText } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    fireEvent.click(getByText('About me'))
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    fireEvent.click(getByText('Contact'))
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    fireEvent.click(getByText('Portraits'))
+
+    expect(mockSetContactSelected).toHaveBeenCalledTimes(3);
+  });
+})
